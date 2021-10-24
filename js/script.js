d39047a5c83b3e95f131c76f5b471cfbc2499392
tuhindewan <tuhinsshadow@gmail.com>
2021-10-24 15:25:01 +0600

    add new books to book list

--- js/script.js
+++ js/script.js
@@ -8,6 +8,32 @@ class Book {
     }
 }
 
+// List class
+
+class List {
+    constructor() {
+        
+    }
+
+    addToBookList(book) {
+        let book_list = document.querySelector('#book-list');
+        let row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href='#' class='delete'>X</a></td>
+        `;
+        book_list.appendChild(row);
+    }
+
+    clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
+}
+
 // Get the form element
 
 let form = document.querySelector("#book-form");
@@ -24,7 +50,9 @@ function newBook(e) {
         isbn = document.querySelector('#isbn').value;
     
     let book = new Book(title, author, isbn);
-    console.log(book);
+    let list = new List();
+    list.addToBookList(book);
+    list.clearFields();
 
     e.preventDefault();
 }
